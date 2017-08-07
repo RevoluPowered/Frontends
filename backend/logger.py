08f23182584d21ef84f8c38e13c069298ac24aec
Gordon MacPherson <gordon.a.macpherson@gmail.com>
2017-08-07 22:25:47 +0100

    added csv_percentage.csv logging which works when server running

--- backend/logger.py
+++ backend/logger.py
@@ -5,10 +5,10 @@ from psutil import cpu_percent
 async def log_cpu():
     """ async logger of CPU usage """
     while True:
-        with open('static/data/cpu_percentage.csv', 'a') as file:
+        with open('./static/data/cpu_percentage.csv', 'a') as file:
             serialiser = csv.writer(file)
             serialiser.writerow(cpu_percent(interval=1, percpu=True))
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(log_cpu())
